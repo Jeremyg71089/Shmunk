525ba1516f94fb190dc67798da3e026dd4b1717f
Jeremyg71089 <Jeremygalarza71089@gmail.com>
2017-07-24 17:12:05 -0700

    Getting Started

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+	private Transform t;
+	// Use this for initialization
+	void Start () {
+		
+		t = this.gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			t.rotation = Quaternion.Euler (0,0,90);
+		}
+		else if (Input.GetKey (KeyCode.RightArrow)) {
+			t.rotation = Quaternion.Euler (0,0,-90);
+		}
+		else if (Input.GetKey (KeyCode.DownArrow)) {
+			t.rotation = Quaternion.Euler (0,0,180);
+		}
+		else if (Input.GetKey (KeyCode.UpArrow)) {
+			t.rotation = Quaternion.Euler (0,0,0);
+		}
+
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	public float speed;
+	private Transform t;
+	private Vector3 mov;
+
+	// Use this for initialization
+	void Start () {
+		t = this.gameObject.transform;
+		mov = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKey (KeyCode.W)) {
+			mov = Vector3.up * speed;
+		}
+		else if (Input.GetKey (KeyCode.S)) {
+			mov = Vector3.up * (-1 * speed);
+		}
+		else if (Input.GetKey (KeyCode.A)) {
+			mov = Vector3.right * (-1 * speed);
+		}
+		else if (Input.GetKey (KeyCode.D)) {
+			mov = Vector3.right * speed;
+		}
+		t.Translate (mov);
+		mov = Vector3.zero;
+	}
+}
