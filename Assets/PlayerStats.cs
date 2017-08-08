ac066b32c2517daa65f77bcb193fc78b6158d2c7
Jeremyg71089 <Jeremygalarza71089@gmail.com>
2017-08-07 18:30:02 -0700

    Added new scripts

---
+++ Assets/PlayerStats.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+	private float health;
+	private float shield;
+	private float maxHealth;
+	private float rateOfFire;
+
+	// Use this for initialization
+	void Start () {
+		maxHealth = 100.0f;
+		health = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (health <= 0.0f) {
+			//destroy the ship, game over
+		}
+	}
+}
